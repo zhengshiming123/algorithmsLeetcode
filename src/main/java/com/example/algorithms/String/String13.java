d17595c7bcfa4004b9af2ea9b6a7abf0f0ecebd8
zhengshiming <1>
2018-03-13 11:16:28 +0800

    String

---
+++ src/main/java/com/example/algorithms/String/String13.java
@@ -0,0 +1,17 @@
+package com.example.algorithms.String;
+
+/**
+ * Created by h on 2018/3/13.
+ * 罗马数字转整数
+ */
+public class String13 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int romanToInt(String s) {
+
+        return 0;
+    }
+}
---
+++ src/main/java/com/example/algorithms/String/String14.java
@@ -0,0 +1,17 @@
+package com.example.algorithms.String;
+
+/**
+ * Created by h on 2018/3/13.
+ * 最长公共前缀
+ */
+public class String14 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+
+        return null;
+    }
+}
